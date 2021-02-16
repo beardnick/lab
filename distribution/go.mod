f6105993a44d4db7fa512beb1521463736c5c33c
qianz <1685437606@qq.com>
2021-02-16 13:20:38 +0800

    feat:finish simplest redis lock

--- distribution/go.mod
+++ distribution/go.mod
@@ -2,4 +2,7 @@ module distribution
 
 go 1.13
 
-require github.com/go-redis/redis/v8 v8.5.0
+require (
+	github.com/go-redis/redis/v8 v8.5.0
+	golang.org/x/exp/errors v0.0.0-20210212053707-62dc52270d37
+)
--- distribution/redis_lock.go
+++ distribution/redis_lock.go
@@ -2,51 +2,54 @@ package main
 
 import (
 	"context"
-	"errors"
 	"time"
 
 	"github.com/go-redis/redis/v8"
+	"golang.org/x/exp/errors/fmt"
 )
 
 var rdb = redis.NewClient(&redis.Options{
-	Addr:     "localhost:6379",
-	Password: "", // no password set
-	DB:       0,  // use default DB
+	Addr:     "192.168.0.120:6379",
+	Password: "raspberry@123", // no password set
+	DB:       0,               // use default DB
 })
 
 type RedisLock struct {
 	Key string
 }
 
+// 如果处理超过1分钟，这个锁就会出问题
 func (r RedisLock) Lock() (err error) {
 	set := false
 	for !set {
-		set, err = rdb.SetNX(context.Background(), r.Key, 1, time.Second).Result()
+		set, err = rdb.SetNX(context.Background(), r.Key, 1, time.Minute).Result()
 		if err != nil {
 			return
 		}
 	}
+	return
 }
 
 func (r RedisLock) Unlock() (err error) {
-	set, err := rdb.SetNX(context.Background(), r.Key, 0, time.Second).Result()
+	_, err = rdb.Del(context.Background(), r.Key).Result()
 	if err != nil {
 		return
-	}
-	if !set {
-		err = errors.New("unlock failed: cannot set lock to 0")
 	}
 	return
 }
 
 func main() {
 	l := RedisLock{Key: "test"}
+	fmt.Println("waiting...")
 	err := l.Lock()
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println("runing...")
+	time.Sleep(time.Second * 10)
 	err = l.Unlock()
 	if err != nil {
 		panic(err)
 	}
+	fmt.Println("finish")
 }
