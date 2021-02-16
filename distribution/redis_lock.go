6d439ff5eec49319fa22b753d847d18fd341f6cc
qianz <1685437606@qq.com>
2021-02-16 14:26:58 +0800

    feat:will not del other's lock

--- distribution/redis_lock.go
+++ distribution/redis_lock.go
@@ -2,6 +2,8 @@ package main
 
 import (
 	"context"
+	"math/rand"
+	"strconv"
 	"time"
 
 	"github.com/go-redis/redis/v8"
@@ -9,20 +11,28 @@ import (
 )
 
 var rdb = redis.NewClient(&redis.Options{
-	Addr:     "192.168.0.120:6379",
-	Password: "raspberry@123", // no password set
-	DB:       0,               // use default DB
+	Addr:     "",
+	Password: "", // no password set
+	DB:       0,  // use default DB
 })
 
 type RedisLock struct {
-	Key string
+	Key    string
+	id     int
+	Expire time.Duration
 }
 
-// 如果处理超过1分钟，这个锁就会出问题
 func (r RedisLock) Lock() (err error) {
+	// 这里必须要有随机数种子，不然就会出现重复的随机数
+	rand.Seed(time.Now().Unix())
+	r.id = rand.Int()
+	fmt.Println("id:", r.id)
+	if r.Expire == 0 {
+		r.Expire = time.Minute
+	}
 	set := false
 	for !set {
-		set, err = rdb.SetNX(context.Background(), r.Key, 1, time.Minute).Result()
+		set, err = rdb.SetNX(context.Background(), r.Key, r.id, r.Expire).Result()
 		if err != nil {
 			return
 		}
@@ -31,22 +41,32 @@ func (r RedisLock) Lock() (err error) {
 }
 
 func (r RedisLock) Unlock() (err error) {
+	i, err := rdb.Get(context.Background(), r.Key).Result()
+	if err != nil {
+		err = fmt.Errorf("lock may has expired:%v", err)
+		return
+	}
+	// 如果不是自己创建的锁就不能删除
+	if i != strconv.Itoa(r.id) {
+		return
+	}
 	_, err = rdb.Del(context.Background(), r.Key).Result()
 	if err != nil {
+		err = fmt.Errorf("lock may has expired:%v", err)
 		return
 	}
 	return
 }
 
 func main() {
-	l := RedisLock{Key: "test"}
+	l := RedisLock{Key: "test", Expire: time.Second * 20}
 	fmt.Println("waiting...")
 	err := l.Lock()
 	if err != nil {
 		panic(err)
 	}
 	fmt.Println("runing...")
-	time.Sleep(time.Second * 10)
+	time.Sleep(time.Second * 15)
 	err = l.Unlock()
 	if err != nil {
 		panic(err)
