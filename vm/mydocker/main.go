3997f8ae85c1e59d23de13ba87fe4c1c8f702354
qianz <1685437606@qq.com>
2021-03-18 23:55:15 +0800

    feat:cgroup test

--- vm/mydocker/main.go
+++ vm/mydocker/main.go
@@ -3,13 +3,29 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"os"
 	"os/exec"
 	"syscall"
 )
 
+const cgroupMemMount = "/sys/fs/cgroup/memory"
+
 func main() {
+	if os.Args[0] == "/proc/self/exe" {
+		fmt.Println("current pid", syscall.Getpid())
+		cmd := exec.Command("sh", "-c", `stress --vm-bytes 200m --vm-keep -m 1`)
+		cmd.SysProcAttr = &syscall.SysProcAttr{}
+		cmd.Stdin = os.Stdin
+		cmd.Stdout = os.Stdout
+		cmd.Stderr = os.Stderr
+		err := cmd.Run()
+		if err != nil {
+			log.Fatal(err)
+			os.Exit(1)
+		}
+	}
 	cmd := exec.Command("sh")
 	cmd.SysProcAttr = &syscall.SysProcAttr{
 		Cloneflags: syscall.CLONE_NEWUTS,
