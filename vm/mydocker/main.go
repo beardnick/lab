c32b61f324e42214ca378d9f56af8a0f8349b506
qianz <1685437606@qq.com>
2021-03-20 19:44:52 +0800

    feat:test cgroup

--- vm/mydocker/main.go
+++ vm/mydocker/main.go
@@ -4,9 +4,12 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
 	"log"
 	"os"
 	"os/exec"
+	"path/filepath"
+	"strconv"
 	"syscall"
 )
 
@@ -14,8 +17,10 @@ const cgroupMemMount = "/sys/fs/cgroup/memory"
 
 func main() {
 	if os.Args[0] == "/proc/self/exe" {
+		// container process
 		fmt.Println("current pid", syscall.Getpid())
-		cmd := exec.Command("sh", "-c", `stress --vm-bytes 200m --vm-keep -m 1`)
+		cmd := exec.Command("sh", "-c", `stress --vm-bytes 100m --vm-keep -m 1`)
+		//cmd := exec.Command("sh", "-c", `free -h`)
 		cmd.SysProcAttr = &syscall.SysProcAttr{}
 		cmd.Stdin = os.Stdin
 		cmd.Stdout = os.Stdout
@@ -25,16 +30,27 @@ func main() {
 			log.Fatal(err)
 			os.Exit(1)
 		}
+		return
 	}
-	cmd := exec.Command("sh")
+	// self process
+	cmd := exec.Command("/proc/self/exe")
 	cmd.SysProcAttr = &syscall.SysProcAttr{
-		Cloneflags: syscall.CLONE_NEWUTS,
+		Cloneflags: syscall.CLONE_NEWUTS | syscall.CLONE_NEWPID | syscall.CLONE_NEWNS,
 	}
 	cmd.Stdin = os.Stdin
 	cmd.Stderr = os.Stderr
 	cmd.Stdout = os.Stdout
-	err := cmd.Run()
+	err := cmd.Start()
 	if err != nil {
 		log.Fatal(err)
 	}
+	fmt.Println("pid", cmd.Process.Pid)
+	cgroup := filepath.Join(cgroupMemMount, "memlimit")
+	// create cgroup
+	os.Mkdir(cgroup, 0755)
+	// add current process to cgroup
+	ioutil.WriteFile(filepath.Join(cgroup, "tasks"), []byte(strconv.Itoa(cmd.Process.Pid)), 0644)
+	// limit process resource
+	ioutil.WriteFile(filepath.Join(cgroup, "memory.limit_in_bytes"), []byte("100m"), 0644)
+	cmd.Process.Wait()
 }
