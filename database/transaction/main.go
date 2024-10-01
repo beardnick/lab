e374fb6a7d520a06d8bf6be02987fe8d0e7af2f7
qianz <qianzhoubeard@gmail.com>
2024-10-01 14:49:49 +0800

    feat:add command line options

--- database/transaction/main.go
+++ database/transaction/main.go
@@ -1,5 +1,23 @@
 package main
 
-func main() {
+import (
+	"context"
+	"fmt"
 
+	"github.com/jessevdk/go-flags"
+)
+
+type ServerOptions struct {
+	Host string `short:"h" long:"host" default:"0.0.0.0" description:"database listen host"`
+	Port int    `short:"p" long:"port" default:"13306" description:"database listen port"`
+}
+
+func main() {
+	opt := ServerOptions{}
+	flags.Parse(&opt)
+	server := Server{
+		database: &DataBase{},
+		address:  fmt.Sprintf("%s:%d", opt.Host, opt.Port),
+	}
+	server.start(context.Background())
 }
